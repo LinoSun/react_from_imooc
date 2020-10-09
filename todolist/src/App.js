e973f1f89710f84f2be9f2b6a562488628da593b
LinoSun <913896141@qq.com>
2020-10-09 21:45:18 +0800

    第二章已看完

---
+++ todolist/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    // jsx的语法:在js文件中写html文件
+    <div>
+      lino is handsome
+    </div>
+  );
+}
+
+export default App;
---
+++ todolist/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// App就算是一个小的组件
+import App from './App';
+
+// jsx语法中，如果我们要使用自己创建的组件可以这么写<App />，首字母大写一般就是组件
+ReactDOM.render(
+  // <React.StrictMode>
+    <App />,
+  //</React.StrictMode>
+  document.getElementById('root')
+);
