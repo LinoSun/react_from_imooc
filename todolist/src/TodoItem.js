c978631b7c915f06644e634cba0c4119274f2f1b
LinoSun <913896141@qq.com>
2020-10-12 22:27:23 +0800

    优化TodoList代码

--- todolist/src/TodoItem.js
+++ todolist/src/TodoItem.js
@@ -1,17 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class TodoItem extends Component{
-	constructor(props){
+class TodoItem extends Component {
+	constructor(props) {
 		super(props);
 		// 放在这个地方不放在div里面是为了提高性能
+		// 修改this
 		this.handleClick = this.handleClick.bind(this);
 	}
-    render(){
-		return <div onClick={this.handleClick}>{this.props.content}</div>
-    }
-    handleClick(){
-        this.props.deleteItem(this.props.index);
-    }
+	render() {
+		const {content} = this.props;
+		return (
+			<div onClick={this.handleClick}>
+				{content}
+			</div>
+		)
+	}
+	handleClick() {
+		const {deleteItem,index} = this.props;
+		deleteItem(index);
+	}
 }
 
 export default TodoItem;
--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
-import './style.css'
 import TodoItem from './TodoItem';
+import './style.css'
 
 // function TodoList() {
 class TodoList extends Component {
@@ -12,6 +12,10 @@ class TodoList extends Component {
 			inputValue: "",
 			list: []
 		}
+		// this指向的绑定统一放在页面的顶部组件初始化的时候
+		this.handleItemDelete = this.handleItemDelete.bind(this);
+		this.handleBtnClick = this.handleBtnClick.bind(this);
+		this.handleInputChange = this.handleInputChange.bind(this);
 	}
 	render() {
 		return (
@@ -27,56 +31,60 @@ class TodoList extends Component {
 						className='input'
 						value={this.state.inputValue}
 						// 事件绑定的时候都是需要bind(this)进行操作的
-						onChange={this.handleInputChange.bind(this)}
+						onChange={this.handleInputChange}
 					/>
-					<button onClick={this.handleBtnClick.bind(this)}>提交</button>
+					<button onClick={this.handleBtnClick}>提交</button>
 				</div>
 				<ul>
-					{
-						// 数组都有一个map方法
-						this.state.list.map((item, index) => {
-							// 
-							return (
-								<div>
-									{/* 传值 */}
-									<TodoItem 
-									content={item} 
-									index={index}
-									// 传递方法的时候需要在方法之后绑定bind(this) 来改写this的指向
-									deleteItem={this.handleItemDelete.bind(this)} />
-								</div>
-							)
-						})
-					}
+					{this.getTodoItem()}
 				</ul>
 			</Fragment>
 		)
 	}
-	handleInputChange(e) {
-		// 改变数据的话必须使用专门的方法this.setState进行操作，里面相当于一个字典
-		this.setState({
-			inputValue: e.target.value
-		})
-	}
-	// 新增功能完成
-	handleBtnClick() {
-		this.setState({
-			// ...tihs.state.list展开运算符，把之前的拿出来生成一个全新的数组
-			list: [...this.state.list, this.state.inputValue],
-			inputValue: ''
+	getTodoItem() {
+		// 数组都有一个map方法
+		return this.state.list.map((item, index) => {
+			// 
+			return (
+				// key一定要写在循环的最外层
+				<TodoItem
+					key={index}
+					content={item}
+					index={index}
+					// 传递方法的时候需要在方法之后绑定bind(this) 来改写this的指向
+					deleteItem={this.handleItemDelete} />
+			)
 		})
 	}
 
+	handleInputChange(e) {
+		// 改变数据的话必须使用专门的方法this.setState进行操作，里面相当于一个字典
+		const value = e.target.value;
+		// this.setState( () => ({}) )语法
+		this.setState(() => ({
+			inputValue: value
+		}));
+	}
+
+	// 新增功能完成
+	handleBtnClick() {
+		// 传入prevState是为了避免错误改变state的状态
+		this.setState((prevState) => ({
+			list: [...prevState.list, prevState.inputValue],
+			inputValue: ''
+		}));
+	}
+
 	handleItemDelete(index) {
-		// 定义一个常量，作为list的拷贝，然后改变数组的切片位置
-		const list = [...this.state.list];
-		// 删除下标为index的选项，删除一个，相当于数组中的切片
-		list.splice(index, 1);
-		// 任何状态的改变都是需要调用this.setState完成的
-		this.setState({
-			list: list
-		})
-		console.log(index)
+
+		this.setState((prevState) => {
+			// 定义一个常量，作为list的拷贝，然后改变数组的切片位置
+			const list = [...prevState.list];
+			// 删除下标为index的选项，删除一个，相当于数组中的切片
+			list.splice(index, 1);
+			// 因为写了不止是简单的函数，所以去掉了最外围的括号，然后加上return
+			return { list }
+		});
 	}
 }
 // 写组件之后必须导出组件
